813ae642a111eae911bf724170fe1e532fe609fd
Riner06 <melker@safari.se>
2024-04-15 21:06:57 +0200

    V.02
    Walking and simple animation

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-window.onload=function(){
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   
@@ -10,45 +9,80 @@ window.onload=function(){
   let characterHeight = 32;
   let characterPosX = 430;
   let characterPosY = 300
+  let spriteX = 0;
+  let spriteY = 0;
   
 
-  function drawBackground() {
-    const img = new Image();
-    img.src = "./Sprites/ground.png";
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    }
-  }
-  
-  function drawCharacter(){
-    const imgHero = new Image();
-    imgHero.src = "./Sprites/hero-sheet.png";
-    imgHero.onload = () => {
-      ctx.drawImage(imgHero,0,0,characterWidth,characterHeight,characterPosX,characterPosY,128,128);
-    }
-  }
-  
-  // document.addEventListener('keydown', walk, false);
-  
-  
-  // function walk(event) {
-  //   if(event.key === "w"){
-  //     console.log('w');
-  //     characterPosY --;
-  //   }
-  //   if(event.key === "s"){
-  //     console.log('s');
-  //     characterPosY ++;
-  //   }
-  //   if(event.key === "a"){
-  //     console.log('a');
-  //     characterPosX --;
-  //   }
-  //   if(event.key === "d"){
-  //     console.log('d');
-  //     characterPosX ++;
+  // function drawBackground() {
+  //   const img = new Image();
+  //   img.src = "./Sprites/ground.png";
+  //   img.onload = () => {
+  //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   //   }
   // }
+  
+  // function drawCharacter(){
+  //   const imgHero = new Image();
+  //   imgHero.src = "./Sprites/hero-sheet.png";
+  //   imgHero.onload = () => {
+  //     ctx.drawImage(imgHero,0,0,characterWidth,characterHeight,characterPosX,characterPosY,128,128);
+  //   }
+  // }
+
+  
+const imgGround = new Image();
+imgGround.src = "./Sprites/ground.png";
+
+const imgSky = new Image();
+imgSky.src = "./Sprites/sky.png";
+
+const imgHero = new Image();
+imgHero.src = "./Sprites/hero-sheet.png";
+
+function drawBackground() {
+  if (imgGround.complete) {
+    ctx.drawImage(imgSky, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(imgGround, 0, 0, canvas.width, canvas.height);
+  }
+}
+
+function drawCharacter(){
+  if (imgHero.complete) {
+    ctx.drawImage(imgHero,spriteX,spriteY,characterWidth,characterHeight,characterPosX,characterPosY,128,128);
+  }
+}
+  
+  document.addEventListener('keydown', walk, false);
+  
+  
+  function walk(event) {
+    const speed = 50; // Change this to change the speed of the character
+    
+    if(event.key === "w"){
+      console.log('w');
+      characterPosY -= speed;
+      spriteX = 32;
+      spriteY = 64;
+    }
+    if(event.key === "s"){
+      console.log('s');
+      characterPosY += speed;
+      spriteX = 0;
+      spriteY = 0;
+    }
+    if(event.key === "a"){
+      console.log('a');
+      characterPosX -= speed;
+      spriteX = 32;
+      spriteY = 96;
+    }
+    if(event.key === "d"){
+      console.log('d');
+      characterPosX += speed;
+      spriteX = 32;
+      spriteY = 32;
+    }
+  }
   
   function draw() {
     // Clear the canvas
@@ -59,11 +93,10 @@ window.onload=function(){
     drawCharacter();
     // Call the draw function again
     window.requestAnimationFrame(draw);
-  }
-  
-  // draw()
-  // Start the animation
+  } 
   window.requestAnimationFrame(draw);
+
+  // draw()
+
   
   //  Promise.all([img, imgHero].map(img => new Promise(resolve => img.onload = resolve))).then(() => requestAnimationFrame(draw));
-}
